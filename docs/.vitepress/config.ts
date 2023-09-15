f941580573f21c187efbb2f78523d5d1a60bd876
yulb-dev <737534279@qq.com>
2023-09-15 15:28:57 +0800

    切换搜索框语言

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -16,21 +16,18 @@ export default defineConfig({
     search: {
       provider: 'local',
       options: {
-        locales: {
-          zh: {
-            translations: {
-              button: {
-                buttonText: '搜索文档',
-                buttonAriaLabel: '搜索文档'
-              },
-              modal: {
-                noResultsText: '无法找到相关结果',
-                resetButtonTitle: '清除查询条件',
-                footer: {
-                  selectText: '选择',
-                  navigateText: '切换'
-                }
-              }
+        translations: {
+          button: {
+            buttonText: '搜索',
+            buttonAriaLabel: '搜索文档'
+          },
+          modal: {
+            noResultsText: '无法找到相关结果',
+            resetButtonTitle: '清除查询条件',
+            footer: {
+              selectText: '选择',
+              navigateText: '切换',
+              closeText: '关闭'
             }
           }
         }
